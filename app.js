43971e95f2da81f90b606839828a8f27524fc620
hjtorch <hjtorch@gmail.com>
2023-12-02 17:12:09 -0800

    Update app.js

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'ejs');
 
 app.get("/", (req, res) => res.render('main.html'));
+app.get("/story", (req, res) => res.render('story.html'));
 
 const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
