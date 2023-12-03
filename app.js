ad00d3a8c8bebc2849ecc6e5ad4b73b9d3b415c0
hjtorch <hjtorch@gmail.com>
2023-12-02 16:07:57 -0800

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
 
+app.set('views', __dirname);
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'ejs');
+
 app.get("/", (req, res) => res.render('main.html'));
 
 const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
