b17af7026521776bbe787f8a22d69367a0ef1388
hjtorch <hjtorch@gmail.com>
2023-12-12 19:41:42 -0800

    DB connection

--- app.js
+++ app.js
@@ -1,6 +1,32 @@
 const express = require("express");
+const {MongoClient} = require('mongodb');
+
 const app = express();
 const port = process.env.PORT || 3001;
+
+// Connect to DB 
+async function DBConnection(){
+    const uri = "mongodb://mongodb-1948:27017";
+    const client = new MongoClient(uri);
+    try {
+        // Connect to the MongoDB cluster
+        await client.connect();
+        // Make the appropriate DB calls
+        await  listDatabases(client);
+    } catch (e) {
+        console.error(e);
+    } finally {
+        await client.close();
+    }
+}
+async function listDatabases(client){
+    databasesList = await client.db().admin().listDatabases();
+ 
+    console.log("Databases:");
+    databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+};
+
+DBConnection().catch(console.error);
 
 app.set('views', __dirname);
 app.engine('html', require('ejs').renderFile);
